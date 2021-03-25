84df8e7651cf4946238ca02cb4beccb548ac5b60
nvmelo <nereuvictor1@gmail.com>
2021-03-25 10:54:14 -0300

    criando petshop no index

---
+++ index.js
@@ -0,0 +1,45 @@
+const nomePetshop = 'PETSHOP AVANADE';
+
+let pet = [
+    {
+        nome: 'Costelinha',
+        tipo: 'cachorro',
+        idade: 5,
+        raca: 'Vira-lata',
+        peso: 3,
+        tutor: 'Doug',
+        contato: '(11) 98899-4545',
+        vacinado: true,
+        servicos: ['banho', 'tosa']
+    },
+    {
+        nome: 'Rex',
+        tipo: 'papagaio',
+        idade: 46,
+        raca: 'Imperador',
+        peso: 1,
+        tutor: 'Jose',
+        contato: '(11) 98569-4145',
+        vacinado: true,
+        servicos: ['banho']
+    },
+    {
+        nome: 'Caninha',
+        tipo: 'cachorro',
+        idade: 3,
+        raca: 'Vira-lata',
+        peso: 5,
+        tutor: 'Denis',
+        contato: '(11) 97199-4215',
+        vacinado: true,
+        servicos: ['banho']
+    }
+];
+
+const listarPets = () => {
+    for(let i=0; i<pet.length; i++){
+        console.log(i+1 + ' -->  ' + pet[i].nome);
+    }
+}
+
+listarPets();
